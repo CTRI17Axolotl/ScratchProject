35242d5a1350fa181bd3083a9f8033e249903eed
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2023-07-14 17:52:35 -0700

    Added react dependency

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// require in needed methods
+
+// build module exports
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      path: ,
+      filename: 
+    },
+    mode: 'development',
+}
